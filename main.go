24912750c894caea893bbf937dd97351b673590b
Greaka <mrgreaka@gmail.com>
2018-06-21 02:25:09 +0200

    had to add two other ciphers to ensure backward compatibility on win7/ie

--- main.go
+++ main.go
@@ -258,6 +258,8 @@ func main() {
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 		},
 	}
 	srv := &http.Server{
