d1ecf69194f1a824e0b3be2a1319618caf731be4
greaka <git@greaka.de>
2020-06-02 09:00:14 +0200

    updated to TLS13

--- main.go
+++ main.go
@@ -156,14 +156,12 @@ func main() {
 	mux.HandleFunc("/dashboard", handleDashboard)
 	mux.HandleFunc("/submit", handleSubmitDashboard)
 	cfg := &tls.Config{
-		MinVersion:               tls.VersionTLS12,
+		MinVersion:               tls.VersionTLS13,
 		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 		PreferServerCipherSuites: true,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 		},
 	}
 	srv := &http.Server{
