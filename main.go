de984ddb334271e94d03d2780a618e9bfae7732e
greaka <2805834+greaka@users.noreply.github.com>
2021-07-10 20:57:27 +0200

    remove https

--- main.go
+++ main.go
@@ -147,23 +147,13 @@ func main() {
 	mux.HandleFunc("/invite", handleInvite)
 	mux.HandleFunc("/dashboard", handleDashboard)
 	mux.HandleFunc("/submit", handleSubmitDashboard)
-	cfg := &tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-		PreferServerCipherSuites: true,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-		},
-	}
+	
 	srv := &http.Server{
 		Addr:         ":4040",
 		Handler:      mux,
-		TLSConfig:    cfg,
-		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
 	}
 
 	loglevels.Info("starting up https listener...")
-	loglevels.Error(srv.ListenAndServeTLS(config.CertificatePath, config.PrivateKeyPath))
+	loglevels.Error(srv.ListenAndServe())
 	os.Exit(1)
 }
